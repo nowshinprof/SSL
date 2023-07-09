7f5833a7eb659b634c379e42ee3ae12cba67962d
Nowshin Fatema <122430484+nowshinprof@users.noreply.github.com>
2023-07-09 13:37:11 +0600

    Update exercise3.m

--- exercise3.m
+++ exercise3.m
@@ -11,3 +11,24 @@ xlabel('time');
 ylabel('amplitude');
 title('unit impulse signal');
 
+
+%Unit step signal
+t2 = 0:4;
+x2=ones(1,5);
+subplot(2,3,2);
+plot(t2,x2);
+xlabel('time');
+ylabel('amplitude');
+title('unit step signal');
+
+%Exponential signal
+t3 = 0:1:20;
+x3=exp(-t3);
+subplot(2,3,3);
+plot(t3,x3);
+xlabel('time');
+ylabel('amplitude');
+title('exponential signal');
+
+
+
