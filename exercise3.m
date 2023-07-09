f6f4d230693b84548612f2a98b7cea32ecde334d
Nowshin Fatema <122430484+nowshinprof@users.noreply.github.com>
2023-07-09 14:05:49 +0600

    Update exercise3.m

--- exercise3.m
+++ exercise3.m
@@ -30,5 +30,40 @@ xlabel('time');
 ylabel('amplitude');
 title('exponential signal');
 
+%Unit ramp signal
+t4 = 0:20;
+x4=t4;
+subplot(2,3,4);
+plot(t4,x4);
+xlabel('time');
+ylabel('amplitude');
+title('Unit ramp signal');
+
+%Sinusoidal signal
+A=5;
+F=2;
+t5 = 0.0001:0.001:1;
+x5=A*sin(2*pi*F*t5);
+subplot(2,3,5);
+plot(t5,x5);
+xlabel('time');
+ylabel('amplitude');
+title('sinusoidal signal');
+
+%Random signal
+
+t6 = -10:1:20;
+x6=rand(1,31);
+subplot(2,3,6);
+plot(t6,x6);
+xlabel('time');
+ylabel('amplitude');
+title('Random signal');
+
+
+
+
+
+
 
 
